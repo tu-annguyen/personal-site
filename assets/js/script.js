702e62221bdeaf1bcad9c75db1c7d23821965bd3
Tu-An Nguyen <tunhnguy@ucsc.edu>
2020-09-07 16:08:23 -0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+// Get the about page
+var about = document.getElementById("about-me");
+
+// Get the offset position of the about page
+var sticky = about.offsetTop - navbar.offsetHeight;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
